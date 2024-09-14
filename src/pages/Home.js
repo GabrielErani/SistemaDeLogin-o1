166027ee4b031fdc21aed716e2e523b934096436
GabrielErani <gabrielciriloerani@gmail.com>
2024-09-14 17:07:31 -0300

    L

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,18 @@
 // src/pages/Home.js
 import React from 'react';
+import Header from '../components/Layout/Header';
+import Footer from '../components/Layout/Footer';
 
 function Home() {
   return (
+    <>
+    <Header />
     <div className="text-center mt-10">
       <h1 className="text-4xl font-bold">Bem-vindo à Página Inicial</h1>
       <p className="mt-4 text-lg">Este é o seu aplicativo React.</p>
     </div>
+    <Footer />
+    </>
   );
 }
 
